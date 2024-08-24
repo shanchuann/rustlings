b42c845498b7289807f5eab0969c9847e4e27e39
Bokun Zhang(张铂坤) <141250922+shanchuann@users.noreply.github.com>
2024-08-24 10:35:32 +0800

    Add files via upload

---
+++ 10_modules/modules2.rs
@@ -0,0 +1,27 @@
+// You can bring module paths into scopes and provide new names for them with
+// the `use` and `as` keywords.
+
+#[allow(dead_code)]
+mod delicious_snacks {
+    // TODO: Fix these use statements
+    pub use self::fruits::PEAR as fruit;
+    pub use self::veggies::CUCUMBER as veggie;
+
+    mod fruits {
+        pub const PEAR: &'static str = "Pear";
+        pub const APPLE: &'static str = "Apple";
+    }
+
+    mod veggies {
+        pub const CUCUMBER: &'static str = "Cucumber";
+        pub const CARROT: &'static str = "Carrot";
+    }
+}
+
+fn main() {
+    println!(
+        "favorite snacks: {} and {}",
+        delicious_snacks::fruit,
+        delicious_snacks::veggie,
+    );
+}
